057a5e8c716406537733da710c03ff24d93d53f8
Akmaljon Fayzullayev <akmaljonfayzullayev07@gmail.com>
2023-02-11 10:43:08 +0500

    todo qo'shish imkoniyati qoshildi

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 def home(request):
+    if request.method == "POST":
+        form =KundalikForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
     data = {
-        "kundalik":Kundalik.objects.all()
+        "kundaliklar":Kundalik.objects.all(),
+        "kundalik":KundalikForm()
     }
     return render(request, 'todo.html', data)
+
